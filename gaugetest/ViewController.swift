5c3404643d2d27afd29981b165bf4a5058ce0e0e
Jussi Kuosa <jussiku@unity3d.com>
2017-07-04 17:30:49 +0300

    converted GaugeView to work with Swift 3

--- gaugetest/ViewController.swift
+++ gaugetest/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import GaugeView
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gaugeView: GaugeView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        gaugeView.percentage = 80
+        gaugeView.thickness = 5
+        gaugeView.labelText = "80%"
+        gaugeView.labelFont = UIFont.systemFont(ofSize: 40, weight: UIFontWeightThin)
+        gaugeView.labelColor = UIColor.lightGray
+        gaugeView.gaugeBackgroundColor = UIColor.lightGray
     }
 
     override func didReceiveMemoryWarning() {
